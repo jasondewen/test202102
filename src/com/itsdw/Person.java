abf38e34a9cdc88c2a3c1a2271da1d90bb5308c2
sdw <jasondewen@163.com>
2021-06-06 14:33:55 +0800

    456

---
+++ src/com/itsdw/Person.java
@@ -0,0 +1,15 @@
+package com.itsdw;
+
+import java.io.Serializable;
+
+/**
+ * test git share
+ * @author sdw
+ * @date 2021/6/6  -- 14:31
+ */
+public class Person implements Serializable {
+
+    private String id;
+    private int age;
+    private String addr;
+}
